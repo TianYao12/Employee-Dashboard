39a1d024741bf9115b79c99e9923083cd59da571
Tian Yao <xtyao2015@gmail.com>
2023-05-03 21:35:52 -0400

    added tailwind css and little aesthetic site

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,19 +1,37 @@
-import "./App.css";
+import "./index.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
+  const showEmployees = true;
+  const [role, setRole] = useState("default");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployees ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          ></input>
+          <div className="flex flex-wrap">
+            <Employee
+              name="Aiden"
+              role="Manager"
+              img="https://www.gannett-cdn.com/presto/2020/08/25/USAT/842737a0-5b6c-47bc-adbc-ea5bef879661-Messi_gone.jpg"
+            />
+            <Employee name="Bob" role={role} />
+            <Employee name="Cade" />
+            <Employee name="Darren" />
+            <Employee name="Enice" />
+          </div>
+        </>
+      ) : (
+        <p>not employees</p>
+      )}
     </div>
   );
 }
