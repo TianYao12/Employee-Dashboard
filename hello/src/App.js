3d1cc59e30cd5c501c8272618b92c6647d850196
Tian Yao <xtyao2015@gmail.com>
2023-05-06 07:08:38 -0400

    Modal to edit each employee

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,32 +1,59 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const showEmployees = true;
-  const [role, setRole] = useState("default");
+  const [employees, setEmployees] = useState([
+    {
+      name: "Messi",
+      role: "developer",
+      img: "https://images.pexels.com/photos/2709388/pexels-photo-2709388.jpeg",
+    },
+    {
+      name: "Ronaldo",
+      role: "developer",
+      img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg",
+    },
+    {
+      name: "Neymar",
+      role: "developer",
+      img: "https://images.pexels.com/photos/4926674/pexels-photo-4926674.jpeg",
+    },
+    {
+      name: "Zidane",
+      role: "developer",
+      img: "https://images.pexels.com/photos/4156467/pexels-photo-4156467.jpeg",
+    },
+    {
+      name: "Pele",
+      role: "developer",
+      img: "https://images.pexels.com/photos/4556737/pexels-photo-4556737.jpeg",
+    },
+    {
+      name: "Maradona",
+      role: "developer",
+      img: "https://images.pexels.com/photos/2218786/pexels-photo-2218786.jpeg",
+    },
+  ]);
 
   return (
     <div className="App">
       {showEmployees ? (
         <>
-          <input
-            type="text"
-            onChange={(e) => {
-              console.log(e.target.value);
-              setRole(e.target.value);
-            }}
-          ></input>
-          <div className="flex flex-wrap">
-            <Employee
-              name="Aiden"
-              role="Manager"
-              img="https://www.gannett-cdn.com/presto/2020/08/25/USAT/842737a0-5b6c-47bc-adbc-ea5bef879661-Messi_gone.jpg"
-            />
-            <Employee name="Bob" role={role} />
-            <Employee name="Cade" />
-            <Employee name="Darren" />
-            <Employee name="Enice" />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              console.log(uuidv4());
+              return (
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
           </div>
         </>
       ) : (
